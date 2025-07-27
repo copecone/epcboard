1050f0e898a9b2ab50338f36c2b0f753d64d9b51
copecone <controld3785@gmail.com>
2025-07-27 18:38:12 +0900

    Use SecureRNG by Default

--- src/main/kotlin/io/github/copecone/epcboard/Main.kt
+++ src/main/kotlin/io/github/copecone/epcboard/Main.kt
@@ -2,33 +2,33 @@ package io.github.copecone.epcboard
 
 import io.github.cdimascio.dotenv.dotenv
 import io.github.copecone.epcboard.logger.InitLogger
-import io.github.copecone.epcboard.util.random.RandomOrgRNG
+import io.github.copecone.epcboard.util.random.SecureRNG
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 val dotenv = dotenv()
-val logger = LoggerFactory.getLogger("MainKt")
+val logger: Logger = LoggerFactory.getLogger("MainKt")
 
 internal object Environment {
     val randomOrgAPIKey: String = dotenv["RANDOM_ORG_API_KEY"]
 }
 
 fun main() {
-    InitLogger.info("Initializing Random.ORG RNG")
+    InitLogger.info("Initializing SecureRNG")
 
-    val rng = RandomOrgRNG()
-    rng.pregenerateFloat(10, 0f..100f)
+    val rng = SecureRNG()
+    // rng.pregenerateFloat(10, 0f..100f)
 
     InitLogger.info("Initializing Embedded Server")
     val server = embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         routing {
             get("/random") {
-                call.respondText(rng.getFloat().toString())
-                if (rng.isFloatQueueEmpty) rng.pregenerateFloat(20, 0f..100f)
+                call.respondText(rng.getFloat(0f..100f).toString())
             }
         }
     }
